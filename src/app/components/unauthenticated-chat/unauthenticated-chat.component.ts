856f931955a2582b945fc57a2b2906e800652cd0
Ivana Držaić <Ivana@example.com>
2024-01-12 09:05:15 +0100

    remove

--- src/app/components/unauthenticated-chat/unauthenticated-chat.component.ts
+++ src/app/components/unauthenticated-chat/unauthenticated-chat.component.ts
@@ -9,7 +9,7 @@ import { ChatService } from '../chatService/chat.service';
   styleUrls: ['./unauthenticated-chat.component.scss'],
   providers: [DatePipe]
 })
-export class UnauthenticatedChatComponent implements OnInit {
+export class UnauthenticatedChatComponent {
   messages$: Observable<any[]>;
   newMessage: string = '';
 
@@ -17,19 +17,6 @@ export class UnauthenticatedChatComponent implements OnInit {
     this.messages$ = this.chatService.getMessages();
   }
 
-  ngOnInit() {
-    this.scrollToBottom();
-  }
-
-  ngAfterViewChecked() {
-    this.scrollToBottom();
-  }
-
-  private scrollToBottom(): void {
-    try {
-      window.scrollTo(0, document.body.scrollHeight);
-    } catch (err) { }
-  }
 
   formatTimestamp(timestamp: number): string {
     return this.datePipe.transform(timestamp, 'mediumTime') || '';
